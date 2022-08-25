bd20b3e0828348c1a93cfbe47a239df13ef37242
ahmedjw <ahmedvander@gmail.com>
2022-08-25 21:12:26 +0300

    First Simple program with python

---
+++ Variable.py
@@ -0,0 +1,9 @@
+print("Hi")
+name = input("What is your name?")
+print("Hi",name)
+answer = input("are you Intersted In python  ")
+
+if answer == "yes": 
+    print("Cool")
+else:
+    print("whhops")
