2120fd0083684890728060cfc2ca747d655b99a1
ahmedjw <ahmedvander@gmail.com>
2022-08-25 22:04:33 +0300

    fav City Game

---
+++ FavioriteCity.py
@@ -0,0 +1,11 @@
+from tkinter.messagebox import RETRY
+
+
+def FindFav():
+    Ans = input("What is your favourite City?  ")
+    print("Your Fav city is ",Ans)
+    return Ans
+
+FindFav()
+FindFav()
+FindFav()
