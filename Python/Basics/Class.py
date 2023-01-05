0c125fc0beb6e2b0f6ffeb28f6dbb7d7db570a1a
ahmedjw <ahmedvander@gmail.com>
2023-01-05 21:29:51 +0200

    is palindrome problem with python sdolotion

---
+++ Python/Basics/Class.py
@@ -0,0 +1,9 @@
+class Vishile :
+    def __init__(self,bodyStyle) :
+        self.bodystyle = bodyStyle
+class car(Vishile):
+    def __init__(self,bodyStyle):
+        super.self(bodyStyle)
+        self.wheels = 4
+    def move():
+        print("move")
---
+++ Python/Basics/IsPalindrome.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def validPalindrome(self, s):
+       L = 0
+       R =len(s) - 1
+       while(L < R):
+           if(s[L] != s[R]):
+               return False
+           L += 1
+           R -= 1
+       return True
